d5d29bcf66336a0691182532dd944430aaf97815
Shashikant Pathak <35792373+shashikantpathak@users.noreply.github.com>
2018-11-28 21:50:36 +0530

    Update AWSUpload.js

--- helpers/AWSUpload.js
+++ helpers/AWSUpload.js
@@ -3,17 +3,17 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 
 AWS.config.update({
-    accessKeyId:'AKIAJYIFR26F4ZU4PKQQ',
-    secretAccessKey:'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',
-    region:'ap-south-1'
+    accessKeyId:'',
+    secretAccessKey:'',
+    region:''
 });
 
 const s0 = new AWS.S3({});
 const upload = multer({
     storage: multerS3({
         s3: s0,
-        bucket: 'socialemergency',
-        acl: 'public-read',
+        bucket: '',
+        acl: '',
         metadata: function(req, file, cb){
             cb(null, {fieldName: file.fieldname});
         },

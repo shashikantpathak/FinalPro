2f5b9cc96561fe9c483f52220a37d83ff8d355c2
Shashikant Pathak <35792373+shashikantpathak@users.noreply.github.com>
2018-11-28 21:51:34 +0530

    Update secretFile.js

--- secret/secretFile.js
+++ secret/secretFile.js
@@ -1,14 +1,14 @@
 module.exports={
     facebook:{
-        clientID:'212853156105404',
-        clientSecret:'bbea0b6b1bc4a30bc601807d01ea394d',
+        clientID:'',
+        clientSecret:'',
         
         passReqToCallback: true
     },
     google:{
-        clientID:'772702558825-dlcuqre418vntcuqloodmbaodad416n1.apps.googleusercontent.com',
-        clientSecret:'sSKIw6FaHx1RY39BWzWrRJ3v',
-        callbackURL:'http://localhost:3000/auth/google/callback',
+        clientID:'',
+        clientSecret:'',
+        callbackURL:'',
         passReqToCallback: true
     }
 }

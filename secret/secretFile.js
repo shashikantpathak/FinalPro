d985072e48c8addd162befcaa3e88800cf26ace9
Shashikant <co090@co-090.cssad.compassitesinc.com>
2018-11-28 21:45:27 +0530

    commit

---
+++ secret/secretFile.js
@@ -0,0 +1,14 @@
+module.exports={
+    facebook:{
+        clientID:'212853156105404',
+        clientSecret:'bbea0b6b1bc4a30bc601807d01ea394d',
+        
+        passReqToCallback: true
+    },
+    google:{
+        clientID:'772702558825-dlcuqre418vntcuqloodmbaodad416n1.apps.googleusercontent.com',
+        clientSecret:'sSKIw6FaHx1RY39BWzWrRJ3v',
+        callbackURL:'http://localhost:3000/auth/google/callback',
+        passReqToCallback: true
+    }
+}
